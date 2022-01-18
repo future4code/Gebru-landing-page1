7edc05b2282a667cb4ed750ea085cad8a69b9bdc
junior <celiojunior0110@gmail.com>
2022-01-18 10:25:23 -0300

    :art: Organização na estrutura dos arquivos e código 1- Movido o conteúdo da pasta src para a pasta img 2- Posicionado as sections na ordem 1,2,3... 3- Corrigido o problema da barra de rolagem horizontal aparecendo 4- Links do cabeçalho agora fazem referência às sections

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ function typeWriter(text) {
 	const arrayText = text.innerHTML.split('')
 	text.innerHTML = ''
 	arrayText.forEach((letter, i) => {
-		setTimeout(() => text.innerHTML += letter, 120 * i)
+		setTimeout(() => text.innerHTML += letter, 140 * i)
 	})
 }
 
