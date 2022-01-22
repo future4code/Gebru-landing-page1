0e100ec3882d6497ac45ec2cd8503a892a8ee0c0
pepinodev <92928197+pepinodev@users.noreply.github.com>
2022-01-21 22:17:17 -0300

    Responsividade em andamento

--- js/index.js
+++ js/index.js
@@ -35,3 +35,37 @@ $('a[href^="#"]').on('click', function(e) {
     scrollTop: targetOffset
   }, 500)
 })
+
+// Menu NavBar Responsivo
+class MobileNavbar{
+  constructor(mobileMenu, navList, navLinks){
+    this.mobileMenu = document.querySelector(mobileMenu);
+    this.navList = document.querySelector(navList);
+    this.navLinks = document.querySelectorAll(navLinks);
+    this.activeClass = "active"; 
+
+    this.handleClick = this.handleClick.bind();
+  }
+
+  handleClick(){
+    this.navList.classList.toggle(this.activeClass);
+  }
+
+  addClickEvent(){  
+    this.mobileMenu.addEventListener("click", this.handleClick);
+  }
+
+  init(){
+    if(this.mobileMenu) {
+      this.addClickEvent();
+    }
+    return this;
+  }
+}
+
+const mobileNavbar = new MobileNavbar(
+  ".mobile-menu",
+  ".nav_link",
+  ".nav_link li",
+);
+mobileNavbar.init();
